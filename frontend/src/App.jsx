e18378d267213f9e4f85f13763120e1d7bc7f11b
Bruk <bruktsegaye3@gmail.com>
2026-06-27 18:34:14 +0300

    APP.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import Navbar from "./components/NavBar";   
 import UserForm from "./components/UserForm";
-
+import ProductForm from "./components/ProductForm";
+import ProductTable from "./components/ProductTable";
+import TransactionTable from "./components/TransactonTable";  
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-gray-100 p-8 space-y-10">
+      <Navbar />
       <UserForm />
+      <ProductForm />
+      <ProductTable />
+      <TransactionTable />
     </div>
   );
 }
